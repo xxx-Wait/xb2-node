48c4abfc09973696f7a28339b1f47610fbbe1143
tangjianxin <x5460500@163.com>
2023-01-03 17:53:48 +0800

    在服务端设置响应的头部数据

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+/**
+ * 使用JSON中间件
+ */
+app.use(express.json())
+
 app.listen(port, () => {
     console.log('服务已启动！');
 })
@@ -19,4 +24,23 @@ app.get('/posts/:postId', (request, response) => {
     const { postId } = request.params
     const posts = data.filter(item => item.id == postId)
     response.send(posts[0])
+})
+
+/**
+ * * 创建内容
+ */
+app.post('/posts', (request, response) => {
+    const { content } = request.body
+    console.log('content', content);
+    // 设置响应状态码
+    response.status(201)
+
+    //输出请求头部数据
+    console.log(request.headers['user-token']);
+
+    // 设置响应头部数据
+    response.set('Sing-Along', 'How I wonder what you are!')
+    response.send({
+        message: `成功创建了内容：${content}`
+    })
 })
